76d8a11e436b7a18ef84cc92ce51a2f900412485
lemmark193 <ler.pl@mail.ru>
2023-04-02 01:35:08 +0700

    Добавляет в данные коодринаты объектов для аренды

--- project/src/mocks/offers.ts
+++ project/src/mocks/offers.ts
@@ -1,8 +1,25 @@
-import {Offers} from '../types/offers';
+import {CitiesEnum, Offers} from '../types/offers';
 import {OfferType} from '../constants';
 
+export const City: CitiesEnum = {
+  Amsterdam: {
+    location: {
+      latitude: 52.370216,
+      longitude: 4.895168,
+      zoom: 10,
+    },
+    name: 'Amsterdam',
+  },
+} as const;
+
 export const offers: Offers = [{
+  city: City.Amsterdam,
   id: 0,
+  location: {
+    latitude: 52.3909553943508,
+    longitude: 4.85309666406198,
+    zoom: 10,
+  },
   previewImage: 'img/apartment-01.jpg',
   price: 120,
   rating: 4,
@@ -10,7 +27,13 @@ export const offers: Offers = [{
   type: OfferType.Apartment,
   isPremium: false,
 }, {
+  city: City.Amsterdam,
   id: 1,
+  location: {
+    latitude: 52.3609553943508,
+    longitude: 4.85309666406198,
+    zoom: 10,
+  },
   previewImage: 'img/room.jpg',
   price: 80,
   rating: 4,
@@ -18,7 +41,13 @@ export const offers: Offers = [{
   type: OfferType.Room,
   isPremium: false,
 }, {
+  city: City.Amsterdam,
   id: 2,
+  location: {
+    latitude: 52.3909553943508,
+    longitude: 4.929309666406198,
+    zoom: 10,
+  },
   previewImage: 'img/apartment-02.jpg',
   price: 132,
   rating: 4,
@@ -26,7 +55,13 @@ export const offers: Offers = [{
   type: OfferType.Apartment,
   isPremium: false,
 }, {
+  city: City.Amsterdam,
   id: 3,
+  location: {
+    latitude: 52.3809553943508,
+    longitude: 4.939309666406198,
+    zoom: 10,
+  },
   previewImage: 'img/apartment-03.jpg',
   price: 180,
   rating: 5,
