9aee4e8d366ac07074483f900305e7c12eb47c8d
lemmark193 <ler.pl@mail.ru>
2023-06-04 21:32:42 +0700

    Добавляет оставшиеся тесты асинхронных операций

--- project/src/store/api-actions/api-actions.test.ts
+++ project/src/store/api-actions/api-actions.test.ts
@@ -3,6 +3,9 @@ import MockAdapter from 'axios-mock-adapter';
 import thunk from 'redux-thunk';
 import {configureMockStore} from '@jedmao/redux-mock-store';
 import {APIRoute} from '../../data/constants';
+import {checkAuthAction, fetchOffersAction, loginAction, logoutAction} from './api-actions';
+import {AuthData} from '../../types/auth';
+import {generateMockOffer} from '../../mocks/offers';
 
 import type {Action} from 'redux';
 import type {State} from '../../types/store';
@@ -19,13 +22,108 @@ describe('Async actions', () => {
     ThunkDispatch<State, typeof api, Action>
   >(middlewares);
 
-  it('should `authStatus` is `AUTH` when server returns 200', () => {
-    const store = mockStore();
+  it('checkAuthAction fullfilled test', async () => {
     mockAPI
       .onGet(APIRoute.Login)
       .reply(200, []); // TODO: [] --- wtf?
 
-    expect(store.getActions())
-      .toEqual([]);
+    const store = mockStore();
+
+    expect(store.getActions()).toEqual([]);
+
+    await store.dispatch(checkAuthAction());
+
+    const actions = store.getActions().map(({type}) => type as string);
+
+    expect(actions).toEqual([
+      checkAuthAction.pending.type,
+      checkAuthAction.fulfilled.type,
+    ]);
+  });
+
+  it('loginAction fullfilled test', async () => {
+    const FAKE_EMAIL = 'user@email.ru';
+    const TOKEN = 'secret';
+    const fakeUser: AuthData = {login: FAKE_EMAIL, password: 'password1'};
+
+    mockAPI
+      .onPost(APIRoute.Login)
+      .reply(200, {email: FAKE_EMAIL, token: TOKEN});
+
+    const store = mockStore();
+    Storage.prototype.setItem = jest.fn();
+
+    await store.dispatch(loginAction(fakeUser));
+
+    const actions = store.getActions().map(({type}) => type as string);
+
+    expect(actions).toEqual([
+      loginAction.pending.type,
+      loginAction.fulfilled.type,
+    ]);
+
+    expect(Storage.prototype.setItem).toBeCalledTimes(1);
+    expect(Storage.prototype.setItem).toBeCalledWith('six-sities-token', TOKEN);
+  });
+
+  it('loginAction rejected test', async () => {
+    const fakeUser: AuthData = {login: 'user@email.wrong', password: 'password1'};
+
+    mockAPI
+      .onPost(APIRoute.Login)
+      .reply(400);
+
+    const store = mockStore();
+    Storage.prototype.setItem = jest.fn();
+
+    await store.dispatch(loginAction(fakeUser));
+
+    const actions = store.getActions().map(({type}) => type as string);
+
+    expect(actions).toEqual([
+      loginAction.pending.type,
+      loginAction.rejected.type,
+    ]);
+
+    expect(Storage.prototype.setItem).toBeCalledTimes(0);
+  });
+
+  it('fetchOffersAction fullfilled test', async () => {
+    const fakeOffers = Array.from({length: 3}, generateMockOffer);
+
+    mockAPI
+      .onGet(APIRoute.Offers)
+      .reply(200, fakeOffers);
+
+    const store = mockStore();
+    await store.dispatch(fetchOffersAction());
+
+    const actions = store.getActions().map(({type}) => type as string);
+
+    expect(actions).toEqual([
+      fetchOffersAction.pending.type,
+      fetchOffersAction.fulfilled.type,
+    ]);
+  });
+
+  it('logoutAction fullfilled test', async () => {
+    mockAPI
+      .onDelete(APIRoute.Logout)
+      .reply(204);
+
+    const store = mockStore();
+    Storage.prototype.removeItem = jest.fn();
+
+    await store.dispatch(logoutAction());
+
+    const actions = store.getActions().map(({type}) => type as string);
+
+    expect(actions).toEqual([
+      logoutAction.pending.type,
+      logoutAction.fulfilled.type,
+    ]);
+
+    expect(Storage.prototype.removeItem).toBeCalledTimes(1);
+    expect(Storage.prototype.removeItem).toBeCalledWith('six-sities-token');
   });
 });
